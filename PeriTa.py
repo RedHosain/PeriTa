f62adc2a6005a80b262c336426b05c4dc8d65125
RedHosain <81237419+RedHosain@users.noreply.github.com>
2022-04-22 12:29:38 -0400

    Add files via upload

---
+++ PeriTa.py
@@ -0,0 +1,388 @@
+#PeriTa
+
+print("PeriTa")
+
+inp = str(input("Type...."))
+
+class main():
+	def datapro():
+
+		#Data
+
+		#Hydrogen
+
+
+		h = """Hydrogen\n
+Name                   --> Hydrogen\n
+Symbol                 --> H\n
+Appearance             --> colorless gas\n
+Atomic Number          --> 1\n
+Group                  --> 1\n
+Period                 --> 1\n
+Standard atomic weight --> 1.008 (conventional)\n
+Electrons per shell    --> 1\n
+Phase(at STP)          --> Gas\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.08988 g/L\n
+
+Description:\n
+
+Hydrogen (H), a colourless, odourless, tasteless, \nflammable gaseous substance that is the \nsimplest member of the family of chemical elements.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Hydrogen] [https://www.britannica.com/science/hydrogen]
+"""
+
+		#Helium
+
+		he = """Helium\n
+Name                   --> Helium\n
+Symbol                 --> He\n
+Appearance             --> colorless gas, exhibiting a gray, \n                           cloudy glow (or reddish-orange if an especially \n                           high voltage is used) when placed in an electric field\n
+Atomic Number          --> 2\n
+Group                  --> 18\n
+Period                 --> 1\n
+Standard atomic weight --> 4.002602\n
+Electrons per shell    --> 2\n
+Phase(at STP)          --> Gas\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.1786 g/L\n
+
+Description:\n
+
+Helium is a chemical element with the symbol He \nand atomic number 2. It is a colorless, odorless, tasteless, \nnon-toxic, inert, monatomic gas and the first in the noble gas \ngroup in the periodic table. Its boiling and \nmelting point are the lowest among all the elements.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Helium] [https://www.britannica.com/science/helium-chemical-element]
+"""
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white\n
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+		#Beryllium
+
+		be = """Beryllium\n
+Name                   --> Beryllium\n
+Symbol                 --> Be\n
+Appearance             --> white-gray metallic\n
+Atomic Number          --> 4\n
+Group                  --> 2\n
+Period                 --> 2\n
+Standard atomic weight --> 9.0121831\n
+Electrons per shell    --> 2, 2\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 1.85 g/cm^3\n
+
+Description:\n
+
+Beryllium is a silvery-white metal. It is relatively soft and has a low density. \nBeryllium is used in alloys with copper or nickel to make gyroscopes, \nsprings, electrical contacts, spot-welding electrodes and non-sparking tools. \nMixing beryllium with these metals increases their electrical and thermal conductivity.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Beryllium] [https://www.rsc.org/periodic-table/element/4/beryllium]
+"""
+
+		#Boron
+
+		b = """Boron\n
+Name                   --> Boron\n
+Symbol                 --> B\n
+Appearance             --> black-brown\n
+Atomic Number          --> 5\n
+Group                  --> 13\n
+Period                 --> 2\n
+Standard atomic weight --> 10.806, 10.821 (conventional: 10.81)\n
+Electrons per shell    --> 2, 3\n
+Phase(at STP)          --> Solid\n
+Block                  --> p-block\n
+Density(at STP)        --> 2.08 g/cm3(liquid)\n
+
+Description:\n
+
+Boron is a non metallic element and the only non-metal of the group 13 of the \nperiodic table the elements. Boron is electron-deficient, possessing a vacant \np-orbital. It has several forms, the most common of which is amorphous \nboron, a dark powder, unreactive to oxygen, water, acids and alkalis.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Boron] [https://www.lenntech.com/periodic/elements/b.htm]
+"""
+
+		#Carbon
+
+		c = """Carbon\n
+Name                   --> Carbon\n
+Symbol                 --> C\n
+Appearance             --> graphite: black, metallic-looking \n                           diamond: clear
+Atomic Number          --> 6\n
+Group                  --> 14\n
+Period                 --> 2\n
+Standard atomic weight --> 12.0096, 12.0116 (conventional: 12.011)\n
+Electrons per shell    --> 2, 4\n
+Phase(at STP)          --> Solid\n
+Block                  --> p-block\n
+Density(at STP)        --> amorphous: 1.8–2.1 g/cm^3 \n                           graphite: 2.267 g/cm^3 \n                           diamond: 3.515 g/cm^3\n
+
+Description:\n
+
+Carbon is a chemical element with the symbol C and atomic number 6. \nIt is nonmetallic and tetravalent—making four electrons available to form covalent chemical bonds. \nIt belongs to group 14 of the periodic table. Carbon makes up only about 0.025 percent of Earth's crust.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Carbon]
+"""
+
+		#Nitrogen
+
+		n = """Nitrogen\n
+Name                   --> Nitrogen\n
+Symbol                 --> N\n
+Appearance             --> colorless gas, liquid or solid\n
+Atomic Number          --> 7\n
+Group                  --> 15\n
+Period                 --> 2\n
+Standard atomic weight --> 14.00643, 14.00728 (conventional: 14.007)\n
+Electrons per shell    --> 2, 5\n
+Phase(at STP)          --> Gas\n
+Block                  --> p-block\n
+Density(at STP)        --> 1.2506 g/L at 0 °C, 1013 mbar\n
+
+Description:\n
+
+nitrogen (N), nonmetallic element of Group 15 of the periodic table. \nIt is a colourless, odourless, tasteless gas that is the most \nplentiful element in Earth's atmosphere and is a constituent of all living matter. nitrogen.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Nitrogen] [https://www.britannica.com/science/nitrogen]
+"""
+
+
+		#Oxygen
+
+		o = """Oxygen\n
+Name                   --> Oxygen\n
+Symbol                 --> O\n
+Appearance             --> gas: colorless liquid and solid: pale blue\n
+Atomic Number          --> 8\n
+Group                  --> 16\n
+Period                 --> 2\n
+Standard atomic weight --> 15.99903, 15.99977 (conventional: 15.999)\n
+Electrons per shell    --> 2, 6\n
+Phase(at STP)          --> Gas\n
+Block                  --> p-block\n
+Density(at STP)        --> 1.429 g/L\n
+
+Description:\n
+
+Oxygen is a colourless, odourless, tasteless gas essential to living organisms, \nbeing taken up by animals, which convert it to carbon dioxide; plants, \nin turn, utilize carbon dioxide as a source of carbon and return \nthe oxygen to the atmosphere.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Oxygen] [https://www.britannica.com/science/oxygen]
+"""
+
+
+		#Fluorine
+
+		f = """Fluorine\n
+Name                   --> Fluorine\n
+Symbol                 --> F\n
+Appearance             --> gas: very pale yellow\n                           liquid: bright yellow \n                           solid: alpha is opaque, beta is transparent\n
+Atomic Number          --> 9\n
+Group                  --> 17\n
+Period                 --> 2\n
+Standard atomic weight --> 18.998403163\n
+Electrons per shell    --> 2, 7\n
+Phase(at STP)          --> Gas\n
+Block                  --> p-block\n
+Density(at STP)        --> 1.696 g/L\n
+
+Description:\n
+
+Fluorine is a chemical element with the symbol F and atomic number 9\n. It is the lightest halogen and exists at standard conditions as a highly toxic, \npale yellow diatomic gas. As the most electronegative element, \nit is extremely reactive, as it reacts with all other elements except for argon, \nneon, and helium.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Fluorine]
+"""
+
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+
+		#Lithium
+
+		li = """Lithium\n
+Name                   --> Lithium\n
+Symbol                 --> Li\n
+Appearance             --> silvery-white
+Atomic Number          --> 3\n
+Group                  --> 1\n
+Period                 --> 2\n
+Standard atomic weight --> 6.938, 6.997 (conventional: 6.94)\n
+Electrons per shell    --> 2, 1\n
+Phase(at STP)          --> Solid\n
+Block                  --> s-block\n
+Density(at STP)        --> 0.534 g/cm^3\n
+
+Description:\n
+
+Lithium is a soft, silvery-white, metal that heads group 1, \nthe alkali metals group, of the periodic table of the elements. \nIt reacts vigorously with water. Storing it is a problem. It cannot be kept under oil, \nas sodium can, because it is less dense and floats.\n
+
+Learn More...
+[https://en.wikipedia.org/wiki/Lithium] [https://www.rsc.org/periodic-table/element/3/lithium]
+"""
+
+
+		#Main
+
+		if inp == "H" or inp == "Hydrogen" or inp == "h" or inp == "hydrogen" or inp == "1":
+			print(h)
+		elif inp == "He" or inp == "he" or inp == "Helium" or inp == "helium" or inp == "2":
+			print(he)
+		elif inp == "Li" or inp == "li" or inp == "Lithium" or inp == "lithium" or inp == "3":
+			print(li)
+		elif inp == "Be" or inp == "be" or inp == "Beryllium" or inp == "beryllium" or inp == "4":
+			print(be)
+		elif inp == "B" or inp == "b" or inp == "Boron" or inp == "boron" or inp == "5":
+			print(b)
+		elif inp == "C" or inp == "c" or inp == "Carbon" or inp == "carbon" or inp == "6":
+			print(c)
+		elif inp == "N" or inp == "n" or inp == "Nitrogen" or inp == "nitrogen" or inp == "7":
+			print(n)
+		elif inp == "O" or inp == "o" or inp == "Oxygen" or inp == "oxygen" or inp == "8":
+			print(o)
+		elif inp == "F" or inp == "f" or inp == "Fluorine" or inp == "fluorine" or inp == "9":
+			print(f)
+		else:
+			print("Nothing Found.....Type Again or Type 'Help'") 
+
+	datapro()
